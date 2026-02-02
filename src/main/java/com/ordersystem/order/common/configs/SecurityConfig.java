775db7112242b755bd621599610c71ba35bdf366
jeonghuny <wnele@naver.com>
2026-02-02 14:40:08 +0900

    at생성 url 인증 제외 처리

--- src/main/java/com/ordersystem/order/common/configs/SecurityConfig.java
+++ src/main/java/com/ordersystem/order/common/configs/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
 //                지정한 특정 url을 제외한 모든 요청에 대해서 authenticated(인증처리)하겠다 라는 의미.
                 .exceptionHandling(e->e.authenticationEntryPoint(jwtAuthenticationHandler))
-                .authorizeHttpRequests(a->a.requestMatchers("/member/create","/member/doLogin","/product/list").permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(a->a.requestMatchers("/member/create","/member/doLogin","/product/list", "/member/refresh-at").permitAll().anyRequest().authenticated())
                 .build();
     }
 
