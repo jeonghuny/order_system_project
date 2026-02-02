66c77f4c7ca24f0111c021c1e560eb6d0dfd1022
jeonghuny <wnele@naver.com>
2026-02-02 16:25:49 +0900

    Message 보내는 DTO

---
+++ src/main/java/com/ordersystem/order/common/dtos/SseMessageDto.java
@@ -0,0 +1,17 @@
+package com.ordersystem.order.common.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class SseMessageDto {
+    private String receiver;
+    private String sender;
+    private String message;
+}
