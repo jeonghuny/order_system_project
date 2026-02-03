3a386c3d9f94e840f0f934eda5aae2ab02379da4
jeonghuny <wnele@naver.com>
2026-02-03 11:01:21 +0900

    bean객체간의 DI는 "메서드 파라미터 주입" 이 가능

--- src/main/java/com/ordersystem/order/common/configs/RedisConfig.java
+++ src/main/java/com/ordersystem/order/common/configs/RedisConfig.java
@@ -8,6 +8,9 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -33,8 +36,10 @@ public class RedisConfig {
     @Bean
     @Qualifier("rtInventory")
 //    모든 template중에 무조건 redisTemplate이라는 메서드명이 반드시 1개는 있어야함.
+//    bean객체 생성시, bean객체간에 DI(의존성주입)는 "메서드 파라미터 주입" 이 가능
     public RedisTemplate<String, String> redisTemplate(@Qualifier("rtInventory") RedisConnectionFactory redisConnectionFactory){
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        // 직렬화 string으로 만드는 툴을 쓰겠다.
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory);
@@ -61,5 +66,45 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
+    }
+
+    @Bean
+    @Qualifier("ssePubSub")
+    public RedisConnectionFactory ssePubSubConnectionFactory(){
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+//        redis pub/sub기능은 db에 값을 저장하는 기능이 아니므로, 특정 db에 의존적이지 않음.
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    @Qualifier("ssePubSub")
+    public RedisTemplate<String, String> ssePubSubRedisTemplate(@Qualifier("ssePubSub") RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+    
+    
+//    redis 리스너(subscribe) 객체
+//    호출구조 : RedisMessageListenerContainer -> MessageListenerAdapter -> SseAlarmService(MessageListener)
+    @Bean
+    @Qualifier("ssePubSub")
+    public RedisMessageListenerContainer redisMessageListenerContainer(@Qualifier("ssePubSub") RedisConnectionFactory redisConnectionFactory
+            , @Qualifier("ssePubSub")MessageListenerAdapter messageListenerAdapter){
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(messageListenerAdapter, new PatternTopic("order-channel"));
+        return container;
+    }
+    
+//    redis에서 수신된 메시지를 처리하는 객체
+    @Bean
+    @Qualifier("ssePubSub")
+    public MessageListenerAdapter messageListenerAdapter(){
+        return new MessageListenerAdapter();
     }
 }
