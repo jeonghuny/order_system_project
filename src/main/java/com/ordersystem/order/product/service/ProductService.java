4b15309ade88e146319fdf42088f48c05b23150a
jeonghuny <wnele@naver.com>
2026-02-04 16:50:04 +0900

    Product 수정 메소드(특히 파일수정)

--- src/main/java/com/ordersystem/order/product/service/ProductService.java
+++ src/main/java/com/ordersystem/order/product/service/ProductService.java
@@ -7,6 +7,7 @@ import com.ordersystem.order.product.dto.ProductCreateDto;
 
 import com.ordersystem.order.product.dto.ProductResDto;
 import com.ordersystem.order.product.dto.ProductSearchDto;
+import com.ordersystem.order.product.dto.ProductUpdateDto;
 import com.ordersystem.order.product.repository.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -58,10 +59,10 @@ public class ProductService {
 
     public Long save(ProductCreateDto productCreateDto) {
         String email = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
-        Member member = memberRepository.findByEmail(email).orElseThrow(()->new EntityNotFoundException("member is not found"));
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException("member is not found"));
         Product product = productRepository.save(productCreateDto.toEntity(member));
-        if(productCreateDto.getProductImage() != null){
-            String fileName = "product-"+product.getId()+"-"+productCreateDto.getProductImage().getOriginalFilename();
+        if (productCreateDto.getProductImage() != null) {
+            String fileName = "product-" + product.getId() + "-" + productCreateDto.getProductImage().getOriginalFilename();
             PutObjectRequest request = PutObjectRequest.builder()
                     .bucket(bucket)
                     .key(fileName)
@@ -72,29 +73,29 @@ public class ProductService {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            String imgUrl = s3Client.utilities().getUrl(a->a.bucket(bucket).key(fileName)).toExternalForm();
+            String imgUrl = s3Client.utilities().getUrl(a -> a.bucket(bucket).key(fileName)).toExternalForm();
             product.updateProfileImageUrl(imgUrl);
         }
 
 //        동시성 문제 해결을 위한 상품등록시 redis에 재고세팅
-        redisTemplate.opsForValue().set(String.valueOf(product.getId()),String.valueOf(product.getStockQuantity()));
+        redisTemplate.opsForValue().set(String.valueOf(product.getId()), String.valueOf(product.getStockQuantity()));
         return product.getId();
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductResDto> findAll(Pageable pageable, ProductSearchDto searchDto){
+    public Page<ProductResDto> findAll(Pageable pageable, ProductSearchDto searchDto) {
         Specification<Product> specification = new Specification<Product>() {
             @Override
             public Predicate toPredicate(Root<Product> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
                 List<Predicate> predicateList = new ArrayList<>();
-                if(searchDto.getProductName() != null){
-                    predicateList.add(criteriaBuilder.like(root.get("name"), "%"+searchDto.getProductName()+"%"));
+                if (searchDto.getProductName() != null) {
+                    predicateList.add(criteriaBuilder.like(root.get("name"), "%" + searchDto.getProductName() + "%"));
                 }
-                if(searchDto.getCategory() != null){
+                if (searchDto.getCategory() != null) {
                     predicateList.add(criteriaBuilder.equal(root.get("category"), searchDto.getCategory()));
                 }
                 Predicate[] predicateArr = new Predicate[predicateList.size()];
-                for (int i=0; i<predicateArr.length; i++){
+                for (int i = 0; i < predicateArr.length; i++) {
                     predicateArr[i] = predicateList.get(i);
                 }
                 Predicate predicate = criteriaBuilder.and(predicateArr);
@@ -102,12 +103,51 @@ public class ProductService {
             }
         };
         Page<Product> postList = productRepository.findAll(specification, pageable);
-        return postList.map(p->ProductResDto.fromEntity(p));
+        return postList.map(p -> ProductResDto.fromEntity(p));
     }
 
     @Transactional(readOnly = true)
-    public ProductResDto findById(Long id){
-        Product product = productRepository.findById(id).orElseThrow(()->new EntityNotFoundException("상품정보없음"));
+    public ProductResDto findById(Long id) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("상품정보없음"));
         return ProductResDto.fromEntity(product);
+    }
+
+    public void update(Long id, ProductUpdateDto dto) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("없는 아이디 입니다."));
+        product.updateProduct(dto);
+
+        if (dto.getProductImage() != null) {
+//            이미지를 수정하는 경우 : 삭제 후 추가
+//            기존이미지를 파일명으로 삭제
+            if (product.getImagePath() != null) {
+                String imgUrl = product.getImagePath();
+                String fileName = imgUrl.substring(imgUrl.lastIndexOf("/") + 1);
+                s3Client.deleteObject(a -> a.bucket(bucket).key(fileName));
+            }
+
+//            신규이미지 등록
+            String newFileName = "product-" + product.getId() + "-" + dto.getProductImage().getOriginalFilename();
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(newFileName)
+                    .contentType(dto.getProductImage().getContentType()) //image/jpeg, video/mp4,....
+                    .build();
+            try {
+//                👉 실제로 S3에 업로드하는 코드 , 파일 바이트를 통째로 S3로 전송
+                s3Client.putObject(request, RequestBody.fromBytes(dto.getProductImage().getBytes()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            String newImageUrl = s3Client.utilities().getUrl(a -> a.bucket(bucket).key(newFileName)).toExternalForm();
+            product.updateProfileImageUrl(newImageUrl);
+        } else {
+//            이미지를 삭제하고자 하는 경우
+            if (product.getImagePath() != null) {
+                String imgUrl = product.getImagePath();
+                String fileName = imgUrl.substring(imgUrl.lastIndexOf("/") + 1);
+                s3Client.deleteObject(a -> a.bucket(bucket).key(fileName));
+            }
+        }
     }
 }
