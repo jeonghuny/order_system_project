d9342e3a238a5bd2a08a386153ec25cd9756486e
jeonghuny <wnele@naver.com>
2026-02-04 17:46:57 +0900

    Repository내 동시성제어방법2. select for update를 통한 락 설정

--- src/main/java/com/ordersystem/order/product/repository/ProductRepository.java
+++ src/main/java/com/ordersystem/order/product/repository/ProductRepository.java
@@ -1,12 +1,23 @@
 package com.ordersystem.order.product.repository;
 
 import com.ordersystem.order.product.domain.Product;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAll(Specification<Product> specification, Pageable pageable);
 
+//    select for update
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+//    Optional<Product> findById(Long id);
+    @Query("select p from Product p where p.id = :id")
+    Optional<Product> findByIdForUpdate(@Param("id") Long id);
 }
